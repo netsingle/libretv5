68a4c094a1c312a6973083a1c911588c859c6a4c
netsingle <160835197+netsingle@users.noreply.github.com>
2025-05-24 00:24:24 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -86,32 +86,26 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
@@ -121,7 +115,6 @@ const API_SITES = {
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
     }
     // 您可以按需添加更多源
 };
